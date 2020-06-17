3aaa7a60c9bf4f3714c413711866cd0602688d2f
abcdefg <choiyurim2@naver.com>
2020-06-18 04:19:48 +0900

    admin post test case 수정

--- bookReview/src/test/java/book/modules/admin/AdminPostControllerTest.java
+++ bookReview/src/test/java/book/modules/admin/AdminPostControllerTest.java
@@ -1,15 +1,22 @@
 package book.modules.admin;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import java.time.LocalDateTime;
 
 import javax.servlet.http.Cookie;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -17,7 +24,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import book.MockMvcTest;
 import book.modules.WithAccount;
 import book.modules.account.Account;
+import book.modules.account.AccountGender;
 import book.modules.account.AccountRepository;
+import book.modules.account.AccountService;
+import book.modules.account.form.AccountForm;
 import book.modules.board.Board;
 import book.modules.board.BoardRepository;
 import book.modules.board.BoardService;
@@ -26,7 +36,10 @@ import book.modules.board.manager.BoardManagerRepository;
 import book.modules.post.Post;
 import book.modules.post.PostRepository;
 import book.modules.post.PostService;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
+@Rollback(value = true)
 @MockMvcTest
 class AdminPostControllerTest {
 
@@ -46,6 +59,9 @@ class AdminPostControllerTest {
 	
 	@Autowired private ObjectMapper objectMapper;
 	
+	@Autowired private PasswordEncoder passwordEncoder;
+	
+	@Autowired private AccountService accountService;
 	
 	private Board testBoard;
 	
@@ -59,7 +75,18 @@ class AdminPostControllerTest {
 	@WithAccount("ididid")
 	@BeforeEach
 	void before() {
-		testAccount = accountRepository.findByLoginId("ididid");
+		
+		String loginId = "choiyurim2";
+		AccountForm form = new AccountForm();
+		form.setLoginId(loginId);
+		form.setBirthYear(1990);
+		form.setPassword(passwordEncoder.encode("12312300"));
+		form.setEmail(loginId+"@naver.com");
+		form.setNickname(loginId+"nickname");
+		form.setPassword("12312344");
+		form.setAccountGender(AccountGender.male);
+		
+		testAccount = accountService.accountCreate(form);
 
 		Board board = Board.builder().role(new SimpleGrantedAuthority("ROLE_USER")).name("기본게시판2").path("default2").build();
 		
@@ -91,9 +118,16 @@ class AdminPostControllerTest {
 
 	}
 	
+	@WithAccount("admintest01")
+	@DisplayName("어드민 페이지 게시글 관리 메인")
 	@Test
-	void test() {
-		//fail("Not yet implemented");
+	void adminPostList() throws Exception {
+		
+		mockMvc.perform(get("/admin/post"))
+		.andExpect(status().isOk())
+		.andExpect(model().attributeExists("postList"))
+		.andExpect(model().attributeExists("keyword"))
+		.andExpect(view().name("admin/post/post"));
 	}
 
 }
